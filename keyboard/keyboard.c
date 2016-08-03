33bef702924c7a8550fefe69daafaccdcca5b937
eMakerStudio <emaker.studio@gmail.com>
2016-08-03 09:33:52 +0200

    initial source commit

---
+++ keyboard/keyboard.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+  /* Varible declaration to hold the input from the keyboard */
+  int age;
+  /* Ask the user for input */
+  printf("How old are you? ");
+  /* Read the number that comes from the keyboad input */
+  scanf("%d", &age);
+  /* Print the users input on the screen */
+  printf("You are %d years old. \n", age);
+  return 0;
+}
+
+
